e8316107f90813c2c6c74f7b1ca16bdc279559d9
Mohit Arya <mohit.arya1211@gmail.com>
2021-07-08 02:25:31 +0530

    Function For Linked List Added

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -21,7 +21,7 @@ void print(Node *head){
     }
 }
 
-Node* userInput(){
+Node* userInput(){  //O(n)
     int data;
     cin>>data;
     Node *head = NULL;
@@ -42,36 +42,112 @@ Node* userInput(){
     return head;
 }
 
+Node* userInput_Optimized(){ //O(1)
+    int data;
+    cin>>data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+Node* insertNodeat(Node *head, int i, int data){
+    Node *newNode = new Node(data);
+    int count = 0;
+    Node *temp = head;
+    if(i == 0){
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+    while(temp!=NULL && count < i-1){
+        temp = temp->next;
+        count++;
+    }
+    //Creating a Temp2 pointer
+    // Node *temp2 = temp->next;
+    // temp->next = newNode;
+    // newNode->next = temp2;
+
+    //Alter w/o Temp2
+    newNode->next = temp->next;
+    temp->next=newNode;
+    return head;
+}
+
+Node* deleteNodeat(Node *head, int i){
+    int count = 0;
+    Node *temp = head;
+    if(i == 0){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    while(temp!=NULL && count < i-1){
+        temp = temp->next;
+        count++;
+    }
+    //Creating a Temp2 pointer
+    Node *temp2 = temp->next;
+    Node *temp3 = temp2->next;
+    temp->next = temp3;
+    delete temp2;
+    return head;
+}
+
 int main()
 {
- // Manual Creation:
-    //Statical Creation:
-    // Node n1(1);
-    // Node *heads = &n1;
-    // Node n2(2);
-    // n1.next = &n2;
+//  Manual Creation:
+    // Statical Creation:
+    // Node s1(1);
+    // Node *heads = &s1;
+    // Node s2(2);
+    // s1.next = &s2;
     // cout<<heads->data;
 
     //Dynamical Creation:
-    // Node *ni = new Node(10);
-    // Node *headd = ni;
-    // Node *nl = new Node(20);
-    // ni->next = nl;
+    // Node *d1 = new Node(10);
+    // Node *headd = d1;
+    // Node *d2 = new Node(20);
+    // d1->next = d2;
 
 
     //Testing Linked List:
-    // Node n1(1);
-    // Node *heads = &n1;
-    // Node n2(2);
-    // Node n3(3);
-    // Node n4(4);
-    // Node n5(5);
-    // n1.next = &n2;
-    // n2.next = &n3;
-    // n3.next = &n4;
-    // n4.next = &n5;
-    // print(heads);
+    // Node t1(1);
+    // Node *headt1 = &t1;
+    // Node t2(2);
+    // Node t3(3);
+    // Node t4(4);
+    // Node t5(5);
+    // t1.next = &t2;
+    // t2.next = &t3;
+    // t3.next = &t4;
+    // t4.next = &t5;
+    // print(headt1);
 
-    Node *head = userInput();
+    //User Input:
+    //Node *head = userInput();
+    //print(head);
+
+    //User Input Optimized:
+    Node *head = userInput_Optimized();
+    print(head);
+    head = insertNodeat(head, 0, 15);
+    cout<<"\n";
+    print(head);
+    head = deleteNodeat(head, 0);
+    cout<<"\n";
     print(head);
 }
